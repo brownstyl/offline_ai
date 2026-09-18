fd85fb466edd5e6e91df5bc80e11e8449f071839
godeh <gabbyjessie44@gmail.com>
2026-09-18 03:47:37 +0100

    added features to backend-logic

--- ai_engine/main.py
+++ ai_engine/main.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
